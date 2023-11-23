1d8786683458c85a4713a96aa4985d8b06e5658f
HeyImNavi <143444671+HeyImNavi@users.noreply.github.com>
2023-11-22 20:12:30 -0600

    Create website.php template for us to mess around with and add different stuff to

---
+++ website.php
@@ -0,0 +1,21 @@
+<!--- Group Project ~ CSCI466 																	--->
+<!--- website.php																				--->
+<!--- Aaron Arreola, Calvin Darley, Eli Gallegos, Jason Lan, Tyler Stenberg						--->
+<!--- Purpose: 																					--->
+<!--- This will be the main page of the website where different functions can be used and other --->
+<!--- pages for the assignment can be reached here												--->
+<html>
+	<head>
+		<title> Main Page - Funky Shop </title>
+	</head>
+	<body>
+		<h1> Funky Shop </h1>
+		<!--- Stuff that we should probably include sooner or later --->
+		
+		<!--- Ideally some link to find products --->
+
+		<!--- Get to shopping cart where they can finish transactions, etc... --->
+
+		<!--- Link to login to account (split between user and employee views for the assignment) --->
+	</body>
+</html>
