2c871344532641305e0d2b03d2f9b1451d9166a7
cjdarley <142070570+cjdarley@users.noreply.github.com>
2023-11-25 13:09:41 -0600

    Add files via upload

---
+++ Calvin's SQL User, Products, and Order.sql
@@ -0,0 +1,9 @@
+INSERT INTO Product VALUES('56721469', '32', 'Vitamin C Tablets - Single', '12.99');
+INSERT INTO Product VALUES('68259786', '45', 'Nike Ankle Sock - Single', '04.99');
+INSERT INTO Product VALUES('32184672', '10', 'Fender Stratocaster - MIM', '90.99');
+INSERT INTO Product VALUES('59813587', '14', 'Empty Water Bottle', '17.99');
+INSERT INTO Product VALUES('56842115', '89', 'Coffee Cake', '14.99');
+
+INSERT INTO User VALUES('joshsmith@gmail.com', '8156872541', 'Josh Smith');
+
+INSERT INTO Orders VALUES('56846658', '1450 W Northern St', '128.99', '6549875341654135', '2023-11-17', '5', 'joshsmith@gmail.com');
