807935f67fc08de03eaad6674d7cd7d4f225be37
cjdarley <142070570+cjdarley@users.noreply.github.com>
2023-11-27 14:17:14 -0600

    Add files via upload

---
+++ finalProducts.sql
@@ -0,0 +1,22 @@
+INSERT INTO PRODUCT
+VALUES 
+('80439252', '30', 'Ground Coffee Beans', '20.99'),
+('80439253', '10', 'Espresso Cups', '10.99'),
+('80439254', '10', 'Cute Aprons', '15.99'),
+('80439255', '69', 'Half-Eaten Bread', '27.99'),
+('80439256', '43', 'Ice Cubes', '12.99'),
+('56721469', '32', 'Vitamin C Tablets - Single', '12.99'),
+('68259786', '45', 'Nike Ankle Sock - Single', '04.99'),
+('32184672', '10', 'Fender Stratocaster - MIM', '90.99'),
+('59813587', '14', 'Empty Water Bottle', '17.99'),
+('56842115', '89', 'Coffee Cake', '14.99'),
+('11238962', '15', 'High-performance fishing rod with lightweight design', '89.99'),
+('13457495', '50', 'Stainless steel 4-slice toaster with adjustable toasting settings', '39.95'),
+('28483748', '30', 'Wireless over-ear headphones with noise cancellation', '96.00'),
+('47392804', '40', 'Wireless vacuuming robot', '68.00'),
+('74927933', '50', 'Ball Point Pen', '99.99'),
+('74928990', '20', 'Pencil Lead', '50.99'),
+('74925583', '29', 'Mechanical Pencil', '79.99'),
+('74921439', '30', 'Number 2 Pencils', '25.99')
+('54256586', '45', 'McDonalds Cheeseburger', '10.99'),
+('59874565', '86', 'Aarons Glasses', '20.00');
