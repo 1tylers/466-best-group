bb28128532fdce161e9cba873419612b36b0a5bf
cjdarley <142070570+cjdarley@users.noreply.github.com>
2023-12-04 13:37:47 -0600

    Update store.sql

--- store.sql
+++ store.sql
@@ -30,7 +30,7 @@ CREATE TABLE Orders (
 CREATE TABLE PlacedOrder (
 	Email VARCHAR(40),
 	OrderID CHAR(8),
-	TrackingNo CHAR(20),
+	TrackingNo CHAR(6),
 	Status VARCHAR(20),
 	FOREIGN KEY (Email) REFERENCES User(Email),
 	FOREIGN KEY (OrderID) REFERENCES Orders(OrderID),
