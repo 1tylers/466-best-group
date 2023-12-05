baa4ecbc3d942e7cfd0f47a99fbbbdcabf8732b6
Eg7-3 <143445015+Eg7-3@users.noreply.github.com>
2023-12-04 19:49:11 -0600

    Update finalProducts.sql

--- finalProducts.sql
+++ finalProducts.sql
@@ -1,6 +1,6 @@
 INSERT INTO PRODUCT
 VALUES 
-('80439252', '30', 'Ground Coffee Beans', '20.99'),
+('80439252', '30', 'Ground Coffee Beans', '20lbs of high quality Coffee Beans','20.99'),
 ('80439253', '10', 'Espresso Cups', '10.99'),
 ('80439254', '10', 'Cute Aprons', '15.99'),
 ('80439255', '69', 'Half-Eaten Bread', '27.99'),
@@ -10,10 +10,10 @@ VALUES
 ('32184672', '10', 'Fender Stratocaster - MIM', '90.99'),
 ('59813587', '14', 'Empty Water Bottle', '17.99'),
 ('56842115', '89', 'Coffee Cake', '14.99'),
-('11238962', '15', 'High-performance fishing rod with lightweight design', '89.99'),
-('13457495', '50', 'Stainless steel 4-slice toaster with adjustable toasting settings', '39.95'),
-('28483748', '30', 'Wireless over-ear headphones with noise cancellation', '96.00'),
-('47392804', '40', 'Wireless vacuuming robot', '68.00'),
+('11238962', '15', 'Fishing Rod','High-performance fishing rod with lightweight design', '89.99'),
+('13457495', '50', 'Toaster', 'Stainless steel 4-slice toaster with adjustable toasting settings', '39.95'),
+('28483748', '30', 'Headphones', 'Wireless over-ear headphones with noise cancellation', '96.00'),
+('47392804', '40', 'Claning Robot', 'Wired vacuuming robot', '68.00'),
 ('74927933', '50', 'Ball Point Pen', '99.99'),
 ('74928990', '20', 'Pencil Lead', '50.99'),
 ('74925583', '29', 'Mechanical Pencil', '79.99'),
