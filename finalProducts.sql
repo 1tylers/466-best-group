c7c3dbfaa7fdc69a526ecfbdf28be3880de691f7
Eg7-3 <143445015+Eg7-3@users.noreply.github.com>
2023-12-04 19:56:25 -0600

    Update finalProducts.sql

--- finalProducts.sql
+++ finalProducts.sql
@@ -1,22 +1,22 @@
 INSERT INTO PRODUCT
 VALUES 
 ('80439252', '30', 'Ground Coffee Beans', '20lbs of high quality Coffee Beans','20.99'),
-('80439253', '10', 'Espresso Cups', '10.99'),
-('80439254', '10', 'Cute Aprons', '15.99'),
-('80439255', '69', 'Half-Eaten Bread', '27.99'),
-('80439256', '43', 'Ice Cubes', '12.99'),
-('56721469', '32', 'Vitamin C Tablets - Single', '12.99'),
-('68259786', '45', 'Nike Ankle Sock - Single', '04.99'),
+('80439253', '10', 'Espresso Cups', 'Description here', '10.99'),
+('80439254', '10', 'Cute Aprons', 'Description here', '15.99'),
+('80439255', '69', 'Half-Eaten Bread', 'Description here', '27.99'),
+('80439256', '43', 'Ice Cubes', 'Description here', '12.99'),
+('56721469', '32', 'Vitamin C Tablets - Single', 'Description here', '12.99'),
+('68259786', '45', 'Nike Ankle Sock - Single', 'Description here', '04.99'),
 ('32184672', '10', 'Fender Stratocaster - MIM', '90.99'),
-('59813587', '14', 'Empty Water Bottle', '17.99'),
-('56842115', '89', 'Coffee Cake', '14.99'),
+('59813587', '14', 'Empty Water Bottle', 'Description here', '17.99'),
+('56842115', '89', 'Coffee Cake', 'Description here', '14.99'),
 ('11238962', '15', 'Fishing Rod','High-performance fishing rod with lightweight design', '89.99'),
 ('13457495', '50', 'Toaster', 'Stainless steel 4-slice toaster with adjustable toasting settings', '39.95'),
 ('28483748', '30', 'Headphones', 'Wireless over-ear headphones with noise cancellation', '96.00'),
 ('47392804', '40', 'Claning Robot', 'Wired vacuuming robot', '68.00'),
-('74927933', '50', 'Ball Point Pen', '99.99'),
-('74928990', '20', 'Pencil Lead', '50.99'),
-('74925583', '29', 'Mechanical Pencil', '79.99'),
-('74921439', '30', 'Number 2 Pencils', '25.99')
-('54256586', '45', 'McDonalds Cheeseburger', '10.99'),
-('59874565', '86', 'Aarons Glasses', '20.00');
+('74927933', '50', 'Ball Point Pen', 'Description here', '99.99'),
+('74928990', '20', 'Pencil Lead',  'Description here','50.99'),
+('74925583', '29', 'Mechanical Pencil', 'Description here', '79.99'),
+('74921439', '30', 'Number 2 Pencils', 'Description here', '25.99')
+('54256586', '45', 'McDonalds Cheeseburger', 'Description here', '10.99'),
+('59874565', '86', 'Aarons Glasses', 'Description here', '20.00');
