70035f256fb4080c80757e895120d9d8ea9d7df9
1tylers <109832604+1tylers@users.noreply.github.com>
2023-12-06 08:24:18 -0600

    Update cartPage.php

--- cartPage.php
+++ cartPage.php
@@ -34,9 +34,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
   <head>
     <title>Shopping Cart</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   </head>
   <body>
-  <h1>Shopping Cart</h1>
+
+	<nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #643843">
+   	 		<a class="navbar-brand" href="website.php">Funky Shop</a>
+    		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        		<span class="navbar-toggler-icon"></span>
+    		</button>
+    		<div class="collapse navbar-collapse" id="navbarNav">
+        		<ul class="navbar-nav ml-auto">
+            		<li class="nav-item">
+                		<a class="nav-link" href="website.php">Home</a>
+            		</li>
+            		<li class="nav-item">
+               			 <a class="nav-link" href="login.php">Employee Login</a>
+            		</li>
+            		<li class="nav-item">
+                		<a class="nav-link" href="trackOrder.php">Track Your Order</a>
+            		</li>
+            		<li class="nav-item active">
+                		<a class="nav-link" href="cartPage.php">Cart</a>
+        			</li>
+        		</ul>
+    		</div>
+		</nav>
+
+		<!-- Bootstrap JS and dependencies -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+  <h1>Your Cart</h1>
   <form action="cartPage.php" method="post">
     <?php
   
@@ -95,4 +126,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     </form>
   </body>
 </html>
+
 
